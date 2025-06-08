6488ee36a42f7521b1f699050288ab057ff7a67c
Borislav Angelov <98764167+b-angelov@users.noreply.github.com>
2025-06-08 22:28:17 +0300

    Update data.js

--- src/api/data.js
+++ src/api/data.js
@@ -2,8 +2,8 @@ import * as api from './api.js';
 
 const host = 'http://localhost:3030';
 //const host = process.env.RENDER_URL || 'http://localhost:3030';
-//api.settings.host = 'http://localhost:3030';
-api.settings.host = process.env.RENDER_URL ||'http://localhost:3030';
+api.settings.host = 'http://localhost:3030';
+//api.settings.host = process.env.RENDER_URL ||'http://localhost:3030';
 
 export const login = api.login;
 export const register = api.register;
